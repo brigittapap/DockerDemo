5da6e9c6f69969cce03694d22a0f0e35b9c9e3b4
Brigi <pap.mbrigitta@gmail.com>
2021-06-03 00:01:38 +0300

    initial commit

---
+++ src/main/java/com/springmysql/dockerdemo/DockerdemoApplication.java
@@ -0,0 +1,13 @@
+package com.springmysql.dockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerdemoApplication.class, args);
+	}
+
+}
