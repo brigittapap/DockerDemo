1d791896fd35513c7e4131c74ffac8b823304015
Brigi <pap.mbrigitta@gmail.com>
2021-06-04 00:02:21 +0300

    initialize spring web

--- src/main/java/com/springmysql/dockerdemo/DockerdemoApplication.java
+++ src/main/java/com/springmysql/dockerdemo/DockerdemoApplication.java
@@ -2,9 +2,17 @@ package com.springmysql.dockerdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DockerdemoApplication {
+
+	@GetMapping("/")
+	public String index(){
+		return "Hello! Welcome to Docker Demo application!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerdemoApplication.class, args);
